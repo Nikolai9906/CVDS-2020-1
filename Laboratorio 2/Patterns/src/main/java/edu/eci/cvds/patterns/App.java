b51719b6e74b6268a6f4440a86a82ec08e9b4815
unknown <steven.bermudez@labinfo.is.escuelaing.edu.co>
2020-01-25 09:43:38 -0500

    Lab 02 en proceso

--- Laboratorio 2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Laboratorio 2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		if(args.length == 0){
+			System.out.println( "Welcome SIN NOMBRE"  );
+		}else {
+			System.out.println( "Welcome " + args[0] +"!"  );
+		}
     }
 }
